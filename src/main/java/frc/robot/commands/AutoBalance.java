9caf92e290dda93f8685c9a903e56f0ab9ce92df
Andrew <none>
2023-02-04 14:23:26 -0500

    Added ramp drive auto and balancing  to auto

--- src/main/java/frc/robot/commands/AutoBalance.java
+++ src/main/java/frc/robot/commands/AutoBalance.java
@@ -4,6 +4,11 @@
 
 package frc.robot.commands;
 
+import java.util.function.BooleanSupplier;
+
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.SwerveDrive;
@@ -11,11 +16,25 @@ import frc.robot.subsystems.SwerveDrive;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+
+
+
 public class AutoBalance extends SequentialCommandGroup {
+  
   /** Creates a new AutoBalance. */
-  public AutoBalance(SwerveDrive swerve) {
+  public AutoBalance(SwerveDrive swerve, boolean forward) {
     // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new DriveToPitch(swerve), new WaitCommand(1.925), new Balance(swerve));
+    // addCommands(new FooCommand(), new BarCommand()); 
+    BooleanSupplier atZero = () -> (Math.abs(swerve.getPitch()) <= 1.0); 
+    addCommands(new DriveToPitch(swerve, forward),                     
+                new WaitCommand(.5), 
+                new Balance(swerve, 0.1), 
+                new WaitCommand(.75), 
+                new ConditionalCommand(new PrintCommand("Ballanced"), new Balance(swerve, 0.08), atZero), 
+                new WaitCommand(.75),
+                new ConditionalCommand(new PrintCommand("Ballanced"), new Balance(swerve, 0.06), atZero), 
+                new WaitCommand(.75),
+                new ConditionalCommand(new PrintCommand("Ballanced"), new Balance(swerve, 0.04), atZero)
+                );
   }
 }
--- src/main/java/frc/robot/commands/DriveToPitch.java
+++ src/main/java/frc/robot/commands/DriveToPitch.java
@@ -4,40 +4,36 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.math.controller.ProfiledPIDController;
+
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.SwerveDrive;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class DriveToPitch extends CommandBase {
   /** Creates a new DriveToPitch. */ 
-  private static double pitch_P = 10.0 ;
-  private static double pitch_I = 1.0 ;
-  private static double pitch_D = 0.0 ;
-  
  // private ProfiledPIDController pitchController;
   private SwerveSubsystem swerve; 
-  private double pitchFlag = 0;
+  private double pitchFlag = 0; 
+  private boolean forward;
+  
       //loadConfigs();
 
-  public DriveToPitch(SwerveDrive swerve) {
+  public DriveToPitch(SwerveDrive swerve, boolean forward) {
     // Use addRequirements() here to declare subsystem dependencies. 
     addRequirements(swerve);
     configPIDs(swerve); 
 
    // pitchController.setGoal(Units.degreesToRadians(angle));
-        this.swerve = swerve;
+        this.swerve = swerve; 
+        this.forward = forward;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() { 
-    pitchFlag = swerve.getPitch();
+    pitchFlag = Math.abs(swerve.getPitch());
     swerve.brakeMode();
    // pitchController.reset(Units.degreesToRadians(swerve.getPitch())); 
   }
@@ -45,12 +41,7 @@ public class DriveToPitch extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    //double pitch = Units.degreesToRadians(swerve.getPitch());
-       // double speed = pitchController.calculate( pitch );
-        //setPosition(speed); 
-        swerve.drive(0.2, 0.0, 0.0);
-
-      //  SmartDashboard.putNumber("Error", pitchController.getPositionError()); 
+          swerve.drive((forward ? 0.2 : -0.2), 0.0, 0.0); 
   }
 
   // Called once the command ends or is interrupted.
@@ -64,13 +55,13 @@ public class DriveToPitch extends CommandBase {
   @Override
   public boolean isFinished() { 
     //  return pitchController.atGoal();
-    double pitch = swerve.getPitch(); 
-    SmartDashboard.putNumber("pitchdifference", pitchFlag - pitch);
+    double pitch = Math.abs(swerve.getPitch()); 
+   // SmartDashboard.putNumber("pitchdifference", pitchFlag - pitch);
+     // System.out.println("not flipped");
     if( (pitch - pitchFlag) >= -1.0){ 
-      pitchFlag = pitch;
+      pitchFlag = pitch; 
       return false;
-    }
-
+    } 
     return true; 
   }
    
