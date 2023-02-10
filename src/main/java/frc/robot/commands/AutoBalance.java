5c7f7a24398088f53608e8112849b6cf945ac200
Andrew <none>
2023-02-09 20:00:52 -0500

    Made a better AutoBalance

--- src/main/java/frc/robot/commands/AutoBalance.java
+++ src/main/java/frc/robot/commands/AutoBalance.java
@@ -4,37 +4,122 @@
 
 package frc.robot.commands;
 
-import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
 
-import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.SwerveDrive;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-
-
-
-public class AutoBalance extends SequentialCommandGroup {
-  
-  /** Creates a new AutoBalance. */
-  public AutoBalance(SwerveDrive swerve, boolean forward) {
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand()); 
-    BooleanSupplier atZero = () -> (Math.abs(swerve.getPitch()) <= 1.0); 
-    addCommands(new DriveToPitch(swerve, forward),                     
-                new WaitCommand(.5), 
-                new Balance(swerve, 0.1), 
-                new WaitCommand(.75), 
-                new ConditionalCommand(new PrintCommand("Ballanced"), new Balance(swerve, 0.08), atZero), 
-                new WaitCommand(.75),
-                new ConditionalCommand(new PrintCommand("Ballanced"), new Balance(swerve, 0.06), atZero), 
-                new WaitCommand(.75),
-                new ConditionalCommand(new PrintCommand("Ballanced"), new Balance(swerve, 0.04), atZero)
+/** Add your docs here. */
+public class AutoBalance {
+    protected static final double PITCH_DELTA = 1.0;
+    protected static final double PITCH_DROP_DELTA = 0.1;
+    protected static final double PITCH_BALANCED = 1.5;
+    
+    public static CommandBase Command(SwerveDrive swerve, boolean forward) {
+        DoubleSupplier speedSupplier = new DoubleSupplier() {
+            private double speed = 0.12;
+            public double getAsDouble(){
+              if (speed <= 0.04) {
+                return speed;
+              }
+              speed -= 0.02;
+              return speed;
+            }
+          };
+        return  new SequentialCommandGroup( new DriveToPitch(swerve, forward),
+                                            new DropTrigger(swerve))
+        .andThen(   new SequentialCommandGroup( new Balance(swerve, speedSupplier),
+                                                new DropTrigger(swerve),
+                                                new BalanceTrigger(swerve) ).repeatedly()
                 );
-  }
+    }
+
+    private static class Balance extends CommandBase {
+        private boolean forward;
+
+        private double pitch = 0.0; 
+        private SwerveDrive swerveDrive;
+        private DoubleSupplier speedSupplier;
+        private double speed;
+      
+        public Balance(SwerveDrive swerveDrive, DoubleSupplier speedSupplier) {
+            addRequirements(swerveDrive);
+            this.swerveDrive = swerveDrive; 
+            this.speedSupplier = speedSupplier;
+        }
+    
+        // Called when the command is initially scheduled.
+        @Override
+        public void initialize() { 
+            speed = speedSupplier.getAsDouble();
+            if(swerveDrive.getPitch() > 0){
+            forward = true;
+            }
+            else if(swerveDrive.getPitch() < 0){
+            forward = false; 
+            }
+            pitch = (Math.abs(swerveDrive.getPitch()) - PITCH_DELTA); 
+            swerveDrive.brakeMode();
+        }
+        
+        // Called every time the scheduler runs while the command is scheduled.
+        @Override
+        public void execute() {
+            swerveDrive.drive((forward ? speed : -speed), 0, 0);
+        }
+        
+        // Called once the command ends or is interrupted.
+        @Override
+        public void end(boolean interrupted) { 
+            System.out.println("stop");
+            swerveDrive.drive(0.0, 0, 0);
+        }
+        
+        // Returns true when the command should end.
+        @Override
+        public boolean isFinished() { 
+        
+            return Math.abs(swerveDrive.getPitch()) < pitch; 
+        
+        } 
+    }
+
+    private static class DropTrigger extends CommandBase {
+        private SwerveDrive swerveDrive;
+        private double pitch;
+        public DropTrigger(SwerveDrive swerveDrive){
+            this.swerveDrive = swerveDrive;
+         }
+
+         @Override
+         public void initialize() { 
+            pitch = swerveDrive.getPitch();
+         }
+              
+         // Returns true when the command should end.
+         @Override
+         public boolean isFinished() { 
+            double new_pitch = swerveDrive.getPitch(); 
+            if (Math.abs(new_pitch - pitch) < PITCH_DROP_DELTA) {
+                return true;
+            }
+            pitch = new_pitch;
+            return false; 
+         } 
+    }
+
+    private static class BalanceTrigger extends CommandBase {
+        private SwerveDrive swerveDrive;
+        public BalanceTrigger(SwerveDrive swerveDrive){
+            this.swerveDrive = swerveDrive;
+         }
+              
+         // Returns true when the command should end.
+         @Override
+         public boolean isFinished() { 
+            return Math.abs(swerveDrive.getPitch()) > PITCH_BALANCED;
+         } 
+    }
 }
