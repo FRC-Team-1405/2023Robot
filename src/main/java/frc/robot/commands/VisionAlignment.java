30f032684eea5e961e4e0aa5330f8b0ac4081af0
Andrew <none>
2023-02-16 19:54:51 -0500

    added auto angle orient to vision alignment

--- src/main/java/frc/robot/commands/VisionAlignment.java
+++ src/main/java/frc/robot/commands/VisionAlignment.java
@@ -21,28 +21,36 @@ import frc.robot.subsystems.SwerveSubsystem;
 
 public class VisionAlignment extends CommandBase {
   /** Creates a new VisionAlignment. */ 
-  private static double x_P = 0.1 ;
+    private static double x_P = 0.1 ;
     private static double x_I = 0.0 ;
-    private static double x_D = 0.0 ;
+    private static double x_D = 0.0 ; 
+    private static double z_P = 10.0 ;
+    private static double z_I = 0.0 ;
+    private static double z_D = 0.0 ;
     public Limelight limelight = new Limelight();
     static{
        // loadConfigs();
-    }
-    private PIDController xController;
+    } 
     private SwerveSubsystem swerve; 
+    private PIDController xController; 
+    private ProfiledPIDController zController;  
     private DoubleSupplier forwardSpeed; 
 
     public VisionAlignment(DoubleSupplier forwardSpeed, double angle, SwerveSubsystem swerve) {
-        addRequirements(swerve);
-        
+        addRequirements(swerve); 
         configPIDs(swerve);
-        xController.setSetpoint(angle); 
+        xController.setSetpoint(0); 
+        zController.setGoal(Units.degreesToRadians(angle));
         this.forwardSpeed = forwardSpeed; 
-        this.swerve = swerve;
+        this.swerve = swerve; 
+        zController.enableContinuousInput(-Math.PI, Math.PI); 
+         
     }
     
     public void initialize() { 
         xController.reset(); 
+        zController.reset(swerve.getPose().getRotation().getRadians());
+       
         limelight.setPipeline((byte) 0);
         limelight.setLED(LED.On);
     }
@@ -50,10 +58,12 @@ public class VisionAlignment extends CommandBase {
     public void execute() { 
       if(limelight.hasTarget()){
         double angle = limelight.getTX();
+        double theta = swerve.getPose().getRotation().getRadians();
         double speed = xController.calculate( angle ); 
+        double zSpeed = zController.calculate( theta );
         SmartDashboard.putNumber("Error", xController.getPositionError()); 
         SmartDashboard.putNumber("Speed", speed); 
-        setPosition(speed);}
+        setPosition(speed, zSpeed);}
     }
 
     public boolean isFinished() {
@@ -61,20 +71,20 @@ public class VisionAlignment extends CommandBase {
       }    
 
     public void end(boolean interrupted) {
-        setPosition(0.0); 
+        setPosition(0.0, 0.0); 
         limelight.setPipeline((byte) 1);
         limelight.setLED(LED.Off);
     }
 
-    private void setPosition(double speed) {
-        ChassisSpeeds chassisSpeeds = new ChassisSpeeds(forwardSpeed.getAsDouble(), -speed, 0);
+    private void setPosition(double speed, double zSpeed) {
+        ChassisSpeeds chassisSpeeds = new ChassisSpeeds(forwardSpeed.getAsDouble(), -speed, zSpeed);
         SwerveModuleState[] moduleStates = swerve.getKinematics().toSwerveModuleStates(chassisSpeeds) ;
         swerve.setModuleStates( moduleStates);
     }
 
     private void configPIDs(SwerveSubsystem swerve){
         xController =  new PIDController(x_P, x_I, x_D);
-       // xController.enableContinuousInput(-Math.PI, Math.PI); 
+        zController = new ProfiledPIDController(z_P, z_I, z_D, new TrapezoidProfile.Constraints(swerve.getMaxAngularSpeed(),swerve.getMaxAngularAcceleration()));
        xController.setTolerance(1);
     }
 
