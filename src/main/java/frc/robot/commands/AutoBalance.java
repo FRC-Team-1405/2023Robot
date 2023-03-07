589f829b0bc07cc7073ae97afc5061707dc06159
Anthony Basile <n/a>
2023-03-06 19:23:27 -0500

    adjust autobalance adjust conveyerbelt speed flip vision left/right

--- src/main/java/frc/robot/commands/AutoBalance.java
+++ src/main/java/frc/robot/commands/AutoBalance.java
@@ -5,10 +5,10 @@
 package frc.robot.commands;
 
 import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.sensors.Limelight;
-import frc.robot.sensors.Limelight.LED;
 import frc.robot.subsystems.SwerveDrive;
 
 /** Add your docs here. */
@@ -27,9 +27,6 @@ public class AutoBalance {
     }
 
     public static class Balance extends CommandBase {
-        private boolean forward;
-
-        private Limelight limelight = new Limelight();
         private double targetPitch = 0.0; 
         private SwerveDrive swerveDrive;
         private DoubleSupplier speedSupplier;
@@ -47,9 +44,9 @@ public class AutoBalance {
             this(swerveDrive, 
                  speedSupplier,
                  new DoubleSupplier() {
-                    private double speed = 0.12;
+                    private double speed = 0.16;
                     public double getAsDouble(){
-                    if (speed <= 0.04) {
+                    if (speed <= 0.06) {
                         return speed;
                     }
                     speed -= 0.02;
@@ -57,16 +54,22 @@ public class AutoBalance {
                     }
                 });
         }
-    
+
         // Called when the command is initially scheduled.
         @Override
-        public void initialize() { 
+        public void initialize() {
+            double angle = Math.IEEEremainder(swerveDrive.getGyroAngle(), 360.0) ;
+            double pitch = swerveDrive.getPitch();
+            
+            boolean isFacingForward = (-90.0 < angle) && (angle < 90.0) ? true : false;
+            boolean isPitchedForwared = (pitch > 0) ? true : false;
+
             speed = speedSupplier.getAsDouble();
-            if(swerveDrive.getPitch() > 0){
-            forward = true;
-            }
-            else if(swerveDrive.getPitch() < 0){
-            forward = false; 
+
+            if (   (!isFacingForward &&  isPitchedForwared)
+                || ( isFacingForward && !isPitchedForwared))
+            {
+                speed = -speed;
             }
             
             targetPitch = (Math.abs(swerveDrive.getPitch()) / 3.0); 
@@ -79,7 +82,7 @@ public class AutoBalance {
         // Called every time the scheduler runs while the command is scheduled.
         @Override
         public void execute() {
-            swerveDrive.drive((forward ? speed : -speed), speedSupplier.getAsDouble(), 0);
+            swerveDrive.drive(speed, ySpeed.getAsDouble(), 0);
         }
         
         // Called once the command ends or is interrupted.
@@ -99,11 +102,11 @@ public class AutoBalance {
 
     public static class DropTrigger extends CommandBase {
         private SwerveDrive swerveDrive;
-        private DoubleSupplier speedSupplier;
+        private DoubleSupplier ySpeed;
         private double pitch;
         public DropTrigger(SwerveDrive swerveDrive, DoubleSupplier speedSupplier){
             this.swerveDrive = swerveDrive;
-            this.speedSupplier = speedSupplier;
+            this.ySpeed = speedSupplier;
          }
 
          @Override
@@ -113,7 +116,7 @@ public class AutoBalance {
               
          @Override
          public void execute() {
-            swerveDrive.drive(0, speedSupplier.getAsDouble(), 0);
+            swerveDrive.drive(0, ySpeed.getAsDouble(), 0);
          }
          @Override
          public void end(boolean interrupted) { 
@@ -133,10 +136,10 @@ public class AutoBalance {
 
     public static class BalanceTrigger extends CommandBase {
         private SwerveDrive swerveDrive;
-        private DoubleSupplier speedSupplier;
+        private DoubleSupplier ySpeed;
         public BalanceTrigger(SwerveDrive swerveDrive, DoubleSupplier speedSupplier){
             this.swerveDrive = swerveDrive;
-            this.speedSupplier = speedSupplier;
+            this.ySpeed = speedSupplier;
          }
               
          // Returns true when the command should end.
@@ -147,7 +150,7 @@ public class AutoBalance {
          } 
          @Override
          public void execute() {
-            swerveDrive.drive(0, speedSupplier.getAsDouble(), 0);
+            swerveDrive.drive(0, ySpeed.getAsDouble(), 0);
          }
          @Override
          public void end(boolean interrupted) { 
