e0f5ec9c0d0f92c5e8d64b155b70eb1f54749f16
Eli <elijah.smith@finneyschool.org>
2023-03-11 12:03:11 -0500

    trajectory

--- src/main/java/frc/robot/commands/VisionAlignment.java
+++ src/main/java/frc/robot/commands/VisionAlignment.java
@@ -12,7 +12,7 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.Preferences;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.sensors.Limelight;
@@ -20,81 +20,74 @@ import frc.robot.sensors.Limelight.LED;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class VisionAlignment extends CommandBase {
-  /** Creates a new VisionAlignment. */ 
-    private static double x_P = 0.1 ;
-    private static double x_I = 0.0 ;
-    private static double x_D = 0.0 ; 
-    private static double z_P = 10.0 ;
-    private static double z_I = 0.0 ;
-    private static double z_D = 0.0 ;
+    /** Creates a new VisionAlignment. */
+    private static double x_P = 0.1;
+    private static double x_I = 0.0;
+    private static double x_D = 0.0;
+    private static double z_P = 10.0;
+    private static double z_I = 0.0;
+    private static double z_D = 0.0;
     public Limelight limelight = new Limelight();
-    static{
-       // loadConfigs();
-    } 
-    private SwerveSubsystem swerve; 
-    private PIDController xController; 
-    private ProfiledPIDController zController;  
-    private DoubleSupplier forwardSpeed; 
+    static {
+        // loadConfigs();
+    }
+    private SwerveSubsystem swerve;
+    private PIDController xController;
+    private ProfiledPIDController zController;
+    private DoubleSupplier forwardSpeed;
 
     public VisionAlignment(DoubleSupplier forwardSpeed, double angle, SwerveSubsystem swerve) {
-        addRequirements(swerve); 
+        addRequirements(swerve);
         configPIDs(swerve);
-        xController.setSetpoint(0); 
+        xController.setSetpoint(0);
         zController.setGoal(Units.degreesToRadians(angle));
-        this.forwardSpeed = forwardSpeed; 
-        this.swerve = swerve; 
-        zController.enableContinuousInput(-Math.PI, Math.PI); 
-         
+        this.forwardSpeed = forwardSpeed;
+        this.swerve = swerve;
+        zController.enableContinuousInput(-Math.PI, Math.PI);
+
     }
-    
-    public void initialize() { 
-        xController.reset(); 
+
+    public void initialize() {
+        xController.reset();
         zController.reset(swerve.getPose().getRotation().getRadians());
-       
+
         limelight.setPipeline((byte) 0);
         limelight.setLED(LED.On);
     }
 
-    public void execute() { 
-      if(limelight.hasTarget()){
-        double angle = limelight.getTX();
-        double theta = swerve.getPose().getRotation().getRadians();
-        double speed = xController.calculate( -angle ); 
-        double zSpeed = zController.calculate( theta );
-        SmartDashboard.putNumber("Error", xController.getPositionError()); 
-        SmartDashboard.putNumber("Speed", speed); 
-        setPosition(speed, zSpeed);}
+    public void execute() {
+        if (limelight.hasTarget()) {
+            double angle = limelight.getTX();
+            double theta = swerve.getPose().getRotation().getRadians();
+            double speed = xController.calculate(-angle);
+            double zSpeed = zController.calculate(theta);
+            SmartDashboard.putNumber("Error", xController.getPositionError());
+            SmartDashboard.putNumber("Speed", speed);
+            setPosition(speed, zSpeed);
+        }
     }
 
     public boolean isFinished() {
         return false;
-      }    
+    }
 
     public void end(boolean interrupted) {
-        setPosition(0.0, 0.0); 
+        setPosition(0.0, 0.0);
         limelight.setPipeline((byte) 1);
         limelight.setLED(LED.Off);
     }
 
     private void setPosition(double speed, double zSpeed) {
         ChassisSpeeds chassisSpeeds = new ChassisSpeeds(forwardSpeed.getAsDouble(), speed, zSpeed);
-        SwerveModuleState[] moduleStates = swerve.getKinematics().toSwerveModuleStates(chassisSpeeds) ;
-        swerve.setModuleStates( moduleStates);
+        SwerveModuleState[] moduleStates = swerve.getKinematics().toSwerveModuleStates(chassisSpeeds);
+        swerve.setModuleStates(moduleStates);
     }
 
-    private void configPIDs(SwerveSubsystem swerve){
-        xController =  new PIDController(x_P, x_I, x_D);
-        zController = new ProfiledPIDController(z_P, z_I, z_D, new TrapezoidProfile.Constraints(swerve.getMaxAngularSpeed(),swerve.getMaxAngularAcceleration()));
-       xController.setTolerance(1);
-    }
-
-    private static void loadConfigs(){
-             Preferences.initDouble("VisionAlignment/X/P", x_P);
-             Preferences.initDouble("VisionAlignment/X/I", x_I);
-             Preferences.initDouble("VisionAlignment/X/D", x_D);
-        x_P = Preferences.getDouble("VisionAlignment/X/P", x_P);
-        x_I = Preferences.getDouble("VisionAlignment/X/I", x_I);
-        x_D = Preferences.getDouble("VisionAlignment/X/D",  x_D);
+    private void configPIDs(SwerveSubsystem swerve) {
+        xController = new PIDController(x_P, x_I, x_D);
+        zController = new ProfiledPIDController(z_P, z_I, z_D,
+                new TrapezoidProfile.Constraints(swerve.getMaxAngularSpeed(), swerve.getMaxAngularAcceleration()));
+        xController.setTolerance(1);
     }
 
 }
